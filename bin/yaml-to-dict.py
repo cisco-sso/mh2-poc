a55c3f83bfa05a7260f06de080ffce216ac613c7
David C Wang <dcwangmit01@gmail.com>
2019-02-19 18:35:12 +0000

    Added helper script

---
+++ bin/yaml-to-dict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import yaml
+import sys
+import pprint
+
+with open(sys.argv[1], 'r') as stream:
+    try:
+        pp = pprint.PrettyPrinter(indent=4)
+        d = yaml.safe_load(stream)
+        pp.pprint(d)
+    except yaml.YAMLError as exc:
+        print(exc)
